9960f8482be72b64d28809ddd1e42447b4038db1
INNERLanky <67173298+INNERLanky@users.noreply.github.com>
2020-06-23 17:59:18 -0700

    INNERBot

--- index.js
+++ index.js
@@ -194,20 +194,6 @@ else{message.member.roles.add(warn_1)}
         message.reply('Color Assigned');
         console.log('Color Assigned');
     }
-    else if (message.content == '!color reset' || '!color white') {
-        message.member.roles.remove(RED);
-        message.member.roles.remove(ORANGE);
-        message.member.roles.remove(YELLOW);
-        message.member.roles.remove(GREEN);
-        message.member.roles.remove(DARK_GREEN);
-        message.member.roles.remove(TEAL);
-        message.member.roles.remove(LIGHT_BLUE);
-        message.member.roles.remove(DARK_BLUE);
-        message.member.roles.remove(PURPLE);
-        message.member.roles.remove(PINK);
-        message.reply('Colors Removed');
-        console.log('Color Assigned');
-    }
     else if (message.content == '!roles') {
         console.log(message.guild.roles);
     }  
