fe8e8cf410b11e626f28b2f1f6a38fae54e0a2c0
INNERLanky <67173298+INNERLanky@users.noreply.github.com>
2020-06-24 09:59:09 -0700

    INNERBot

--- index.js
+++ index.js
@@ -26,20 +26,20 @@ console.log("INNERBot_Online");
 const bot = new Discord.Client();
 
 bot.on('message', (message) => {
-    if (msg.content.startsWith("!kick ")) {
-        if (msg.mentions.members.first()) {
-            msg.mentions.members.first.kick().then((member) => {
-                msg.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+    if (message.content.startsWith("!kick ")) {
+        if (message.mentions.members.first()) {
+            message.mentions.members.first.kick().then((member) => {
+                message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
             }).catch(() => {
-                msg.channel.send("I do not have permissions to do this");
+                message.channel.send("I do not have permissions to do this");
             });
         }
-    }else if (msg.content.startsWith("!ban ")) {
-        if (msg.mentions.members.first()) {
-            msg.mentions.members.first.ban().then((member) => {
-                msg.channel.send(":wave: " + member.displayName + " has been successfully banned :point_right: ");
+    }else if (message.content.startsWith("!ban ")) {
+        if (message.mentions.members.first()) {
+            message.mentions.members.first.ban().then((member) => {
+                message.channel.send(":wave: " + member.displayName + " has been successfully banned :point_right: ");
             }).catch(() => {
-                msg.channel.send("I do not have permissions to do this");
+                message.channel.send("I do not have permissions to do this");
             });
         }
     }
