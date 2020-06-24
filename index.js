0fca6163251eb577aa3d3e2951c3cc863cde00a1
INNERLanky <67173298+INNERLanky@users.noreply.github.com>
2020-06-24 10:55:01 -0700

    INNERBot

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ const warn_4 = '725124499267387452';
 const warn_5 = '725124515503538236';
 console.clear
 console.log("INNERBot_Online");
-//add !mute - muted !unmute
 //embed help
-//dm me 5+ warns
+//@me 5+
 
 const bot = new Discord.Client();
 
@@ -35,7 +34,7 @@ bot.on('message', (message) => {
 
         //Then check if user have permissions to do that
         if(!message.member.hasPermission('KICK_MEMBERS')) {
-            message.channel.send('You have no permissions to do that');
+            message.channel.send('You dont have permissions to do that');
             return;
         };
 
@@ -43,21 +42,107 @@ bot.on('message', (message) => {
         let mentionMember = message.mentions.members.first();
         //If user dont mention a member, that show him this error msg
         if(!mentionMember) {
-            message.channel.send('pls mention member witch you need to kick');
+            message.channel.send('mention the member you woud like to kick');
             return;
         }
 
         //Check if your bot can`t kick this user, so that show this error msg 
         if(!mentionMember.kickable) {
-            message.channel.send('I have no permissions to kick this user');
+            message.channel.send('I dont have permissions to kick this user');
             return
         };
 
         //If all steps are completed successfully try kick this user
         mentionMember.kick()
-            .then(() => console.log(`Kicked ${member.displayName}`))
+            .then(() => message.reply('user has been kicked'))
             .catch(console.error);
-    };
+        };
+        
+    if(message.content.includes('!ban')) {
+        if(message.channel.type === 'DM') {
+            //Fist check if message channel is not direct message, because you cant kick out of guide 
+            message.channel.send('This command can use only in guide');
+            return;
+        };
+
+        //Then check if user have permissions to do that
+        if(!message.member.hasPermission('BAN_MEMBERS')) {
+            message.channel.send('You dont have permissions to do that');
+            return;
+        };
+
+        //const a member, wich you need yo kick (its fist mention message member)
+        let mentionMember = message.mentions.members.first();
+        //If user dont mention a member, that show him this error msg
+        if(!mentionMember) {
+            message.channel.send('mention the member you woud like to ban');
+            return;
+        }
+
+        //Check if your bot can`t kick this user, so that show this error msg 
+        if(!mentionMember.bannable) {
+            message.channel.send('I dont have permissions to ban this user');
+            return
+        };
+
+        //If all steps are completed successfully try kick this user
+        mentionMember.ban()
+            .then(() => message.reply('user has been banned'))
+            .catch(console.error);
+        };
+        
+    if(message.content.includes('!warn')) {
+        if(message.channel.type === 'DM') {
+            //Fist check if message channel is not direct message, because you cant kick out of guide 
+            message.channel.send('This command can use only in guide');
+            return;
+        };
+
+        //Then check if user have permissions to do that
+        if(message.member.roles.cache.hasPermission(Admin)) {
+
+            //const a member, wich you need yo kick (its fist mention message member)
+        let mentionMember = message.mentions.members.first();
+        //If user dont mention a member, that show him this error msg
+        if(!mentionMember) {
+            message.channel.send('mention the member you would like to warn');
+            return;
+        }
+
+        //Check if your bot can`t kick this user, so that show this error msg 
+        if(!mentionMember.kickable) {
+            message.channel.send('I dont have permissions to warn this user');
+            return
+        };
+
+        //If all steps are completed successfully try kick this user
+        if(message.member.roles.cache.has(warn_1)) {
+            //warn 1
+            if(message.member.roles.cache.has(warn_2)) {
+              //warn 2
+              if(message.member.roles.cache.has(warn_3)) {
+              //warn 3
+                if(message.member.roles.cache.has(warn_4)) {
+                //warn 4
+                  if(message.member.roles.cache.has(warn_5)) {
+                  //warn 5
+                  //dm me-('Someone Has Sweared over 5 times!')
+          }//warn 5 brackets
+        else {message.member.roles.add(warn_5)}
+      }//warn 4 brackets
+      else {message.member.roles.add(warn_4)}
+    }//warn 3 brackets
+    else {message.member.roles.add(warn_3)}
+  }//warn 2 brackets
+  else {message.member.roles.add(warn_2)}
+}//warn 1 brackets
+else{message.member.roles.add(warn_1)}
+}}
+then(() => message.reply('user has been warned'))
+.catch(console.error);
+return;
+
+        
     var message_content = message.content.toLowerCase();
     for (var i = 0; i < forbiddenWords.length; i++) {
       if (message_content.includes(forbiddenWords[i])) {
