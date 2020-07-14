86b5cd621c0b5c5d6e95a70f57f64fdd96060d25
INNERLanky <67173298+INNERLanky@users.noreply.github.com>
2020-07-14 11:16:48 -0700

    big update 2.0

--- index.js
+++ index.js
@@ -546,7 +546,7 @@ const cooldowns = new Collection();
  * Client Events
  */
 bot.on('ready', () => {
-  console.log('kool');
+  console.log('music ready!');
 });
 bot.on("warn", (info) => console.log(info));
 bot.on("error", console.error);
