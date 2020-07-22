03e398c7121a2a4b85a3fbf6e933177dd5666d66
INNERLanky <67173298+INNERLanky@users.noreply.github.com>
2020-07-22 08:42:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,100 @@ const HelpCommand = 'Use !color + [color name] to change the color of your name.
 console.clear
 console.log("INNERBot_Online");
 
+
+
+//
+//
+//music commands start
+//
+//
+
+  /**
+ * Module Imports
+ */
+const { Client, Collection } = require("discord.js");
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { PREFIX } = require("./config.json");
+
+var bot = new Client({ disableMentions: "everyone" });
+
+bot.commands = new Collection();
+bot.prefix = PREFIX;
+bot.queue = new Map();
+const cooldowns = new Collection();
+
+/**
+ * Client Events
+ */
+bot.on('ready', () => {
+  console.log('music ready!');
+});
+bot.on("warn", (info) => console.log(info));
+bot.on("error", console.error);
+
+/**
+ * Import all commands
+ */
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(join(__dirname, "commands", `${file}`));
+  bot.commands.set(command.name, command);
+}
+
+bot.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (!message.guild) return;
+
+  if (message.content.startsWith(PREFIX)) {
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command =
+      bot.commands.get(commandName) ||
+      bot.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (!cooldowns.has(command.name)) {
+      cooldowns.set(command.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 1) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const timeLeft = (expirationTime - now) / 1000;
+        return message.reply(
+          `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
+        );
+      }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+      command.execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply("There was an error executing that command.").catch(console.error);
+    }
+  }
+});
+
+//
+//
+//music commands end
+//
+//
+
 //setup
-bot.on("message", message => {
+bot.on('message', (message) => {
   if(message.content == ('!setup')) {
     message.reply('setup in progress')
     let role = message.guild.roles.cache.find(x => x.name === "RED");
@@ -150,98 +242,6 @@ bot.on("message", message => {
   
     }
 })
-
-
-
-//
-//
-//music commands start
-//
-//
-
-  /**
- * Module Imports
- */
-const { Client, Collection } = require("discord.js");
-const { readdirSync } = require("fs");
-const { join } = require("path");
-const { PREFIX } = require("./config.json");
-
-var bot = new Client({ disableMentions: "everyone" });
-
-bot.commands = new Collection();
-bot.prefix = PREFIX;
-bot.queue = new Map();
-const cooldowns = new Collection();
-
-/**
- * Client Events
- */
-bot.on('ready', () => {
-  console.log('music ready!');
-});
-bot.on("warn", (info) => console.log(info));
-bot.on("error", console.error);
-
-/**
- * Import all commands
- */
-const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
-for (const file of commandFiles) {
-  const command = require(join(__dirname, "commands", `${file}`));
-  bot.commands.set(command.name, command);
-}
-
-bot.on("message", async (message) => {
-  if (message.author.bot) return;
-  if (!message.guild) return;
-
-  if (message.content.startsWith(PREFIX)) {
-    const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    const command =
-      bot.commands.get(commandName) ||
-      bot.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
-
-    if (!command) return;
-
-    if (!cooldowns.has(command.name)) {
-      cooldowns.set(command.name, new Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 1) * 1000;
-
-    if (timestamps.has(message.author.id)) {
-      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-      if (now < expirationTime) {
-        const timeLeft = (expirationTime - now) / 1000;
-        return message.reply(
-          `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
-        );
-      }
-    }
-
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-    try {
-      command.execute(message, args);
-    } catch (error) {
-      console.error(error);
-      message.reply("There was an error executing that command.").catch(console.error);
-    }
-  }
-});
-
-//
-//
-//music commands end
-//
-//
 
 //help 
 bot.on('message', (message) => {
