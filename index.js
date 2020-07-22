3c63f86ff309332458edb44fe56ae45779af595b
INNERLanky <67173298+INNERLanky@users.noreply.github.com>
2020-07-22 08:35:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 var bot = new Discord.Client();
-const client = new Discord.Client();
 const HelpCommand = 'Use !color + [color name] to change the color of your name. \n<:red:731642274387198023> red \n<:orange:731642274340929608> orange \n<:yellow:731642273871298676> yellow \n<:green:731642274022293616> green \n<:darkgreen:731642272461881375> dark green \n<:teal:731642267101691984> teal \n<:lightblue:731642265054871594> light blue \n<:darkblue:731642264094244965> dark blue \n<:purple:731642261934178477> purple \n<:pink:731642260537475077> pink. \nFor music, use the commands, or what is in the () for short. \n!play = adds a song to the queue \n!pause = pauses the song \n!resume = resumes the song (r) \n!playlist (pl) = plays a playlist \n!loop (l) = loops the song you are listening to \n!np = shows what is now playing (p) \n!queue (q) = shows what is in the queue \n!shuffle = shuffles all songs in the queue \n!volume (v) = changes the volume by % \n!skip (s) = skips the song you are on \n!stop = bot leaves the channel \n \nYou can also use mute, kick, ban, and warn, if you have the permissions to.\n \nView my source code here: https://github.com/innerlanky/innerbot'
 
 //let RED = message.guild.roles.cache.find(role => role.name === "RED");
@@ -24,114 +23,118 @@ console.clear
 console.log("INNERBot_Online");
 
 //setup
-client.on("guildCreate", guild => {
-  console.log('guild joined')
-    guild.roles.create({
+bot.on("message", message => {
+  if(message.content == ('!setup')) {
+    let role = message.guild.roles.cache.find(x => x.name === "RED");
+    if (typeof role === undefined) {
+    message.channel.send('Setup Complete!')
+    console.log('guild joined')
+    message.guild.roles.create({
         data: {
           name: 'warn_1',
           color: '#ff0000',
         },
         reason: 'setup',
       })
-      guild.roles.create({
+      message.guild.roles.create({
         data: {
           name: 'warn_2',
           color: '#ff0000',
         },
         reason: 'setup',
       })
-      guild.roles.create({
+      message.guild.roles.create({
         data: {
           name: 'warn_3',
           color: '#ff0000',
         },
         reason: 'setup',
       })
-      guild.roles.create({
+      message.guild.roles.create({
         data: {
           name: 'warn_4',
           color: '#ff0000',
         },
         reason: 'setup',
       })
-      guild.roles.create({
+      message.guild.roles.create({
         data: {
           name: 'warn_5',
           color: '#ff0000',
         },
         reason: 'setup',
       })
-      guild.roles.create({
+      message.guild.roles.create({
         data: {
           name: 'MUTED',
           color: '#ff0000',
         },
         reason: 'setup',
       })
-        guild.roles.create({
+        message.guild.roles.create({
           data: {
             name: 'RED',
             color: '#ff0000',
           },
           reason: 'setup',
         })
-        guild.roles.create({
+        message.guild.roles.create({
             data: {
               name: 'ORANGE',
               color: '#ff8000',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'YELLOW',
               color: '#ffff00',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'GREEN',
               color: '#00ff00',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'DARKGREEN',
               color: '#009900',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'TEAL',
               color: '#33ff99',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'LIGHTBLUE',
               color: '#00ffff',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'DARKBLUE',
               color: '#3a3aff',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'PURPLE',
               color: '#a736ff',
             },
             reason: 'setup',
           })
-          guild.roles.create({
+          message.guild.roles.create({
             data: {
               name: 'PINK',
               color: '#ff33ff',
@@ -140,6 +143,11 @@ client.on("guildCreate", guild => {
           })
           .then(console.log)("setup complete")
           .catch(console.error);
+    } else {
+    message.channel.send("This server has already been setup!")
+    }
+  
+    }
 })
 
 
